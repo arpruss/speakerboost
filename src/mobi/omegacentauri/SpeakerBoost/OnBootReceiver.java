99810dc5491d51916ba70014adbe859fcd229a94
arpruss <arpruss@2e98e660-aacf-b7d2-df2f-19c17cf57af2>
2012-08-16 01:17:00 +0000

    start on boot, fix

--- src/mobi/omegacentauri/SpeakerBoost/OnBootReceiver.java
+++ src/mobi/omegacentauri/SpeakerBoost/OnBootReceiver.java
@@ -5,14 +5,25 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.preference.PreferenceManager;
 
 public class OnBootReceiver extends BroadcastReceiver {
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		SharedPreferences options = PreferenceManager.getDefaultSharedPreferences(context); 
+		
+		try {
+			if (options.getInt(Options.PREF_WARNED_LAST_VERSION, 0) != context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode) {
+				return;
+			}
+		} catch (NameNotFoundException e) {
+			return;
+		}		
+		
 		Settings settings = new Settings(context, false);
-		settings.load(PreferenceManager.getDefaultSharedPreferences(context));
+		settings.load(options);
 		
 		if (settings.needService()) {
 			Intent i = new Intent(context, SpeakerBoostService.class);
